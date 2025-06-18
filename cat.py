074e30e59c98d71957cdf5f5c93e0ab5498af1d1
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-18 16:20:11 -0400

    add cat.py

--- cat.py
+++ cat.py
@@ -1,7 +1,17 @@
-i = 0
-while i < 3:
-    print("meow")
-    i += 1
+def main():
+    meow(get_number())
 
-for _ in range(3):
-    print("meow")
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+
+
+def get_number():
+    while True:
+        n = int(input("What's n: "))
+        if n > 0:
+            return n
+
+main()
