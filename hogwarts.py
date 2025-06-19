bd1b4225e483d0bb89afe13fde6336ed2b3efe7b
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-18 20:56:48 -0400

    add hogwarts.py

---
+++ hogwarts.py
@@ -0,0 +1,11 @@
+#Below is an example of a dictionary
+
+students = {
+    "Hermoine" : "Gryffindor",
+    "Harry" : "Gryffindor",
+    "Ron" : "Gryffindor",
+    "Draco" : "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep = ", ")
