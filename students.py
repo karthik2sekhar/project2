1bc3b38a8a2f416ad7c2e400a364c05f426626fc
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-21 02:19:18 -0400

    add students.py

--- students.py
+++ students.py
@@ -1,10 +1,8 @@
 import csv
-students = []
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name":row["name"],"home":row["home"]})
+name = input("What's your name: ")
+home = input("What's your home: ")
 
-for student in sorted(students, key=lambda student: student["name"], reverse=True):
-    print(f"{student['name']} was from {student['home']}")
+with open("students.csv", "a", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=["name","home"])
+    writer.writerow({"name": name, "home": home})
