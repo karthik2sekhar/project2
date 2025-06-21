6ae2ebff9aeae3c8eaed0997eb406239fb800de3
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-20 23:03:58 -0400

    add names.py

--- names.py
+++ names.py
@@ -1,7 +1,11 @@
-names = []
+name = input("whats your name: ")
 
-for _ in range(3):
-    names.append(input("What's your name: "))
+"""
+file = open("names.txt", "a")
+file.write(f"{name}\n")
+file.close
+"""
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
 
-for name in sorted(names):
-    print(f"Hello, {name}")
+
