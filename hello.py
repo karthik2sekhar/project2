1be06565fc3c0a7e75a61a873bb0ff1c95c080b9
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-20 22:18:28 -0400

    add test_hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ def main():
     hello(name)
 
 def hello(to="world"):
-    print("Hello,", to)
+    return f"Hello, {to}"
 
-main()
+if __name__ == "__main__":
+    main()
--- test_calculator.py
+++ test_calculator.py
@@ -1,26 +1,12 @@
 from calculator import square
 
-def main():
-    test_calculator()
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
-def test_calculator():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("square of 2 is not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("square of 3 is not 9")
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print("square of -2 is not 4")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("square of 0 is not 0")
+def test_negative():
+    assert square(-2) == 4
 
-if __name__ == "__main__":
-    main()
+def test_zero():
+    assert square(0) == 0
 
