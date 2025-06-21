c1cd7028b99bb35d293b01c6c760c78912fe8f78
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-21 16:05:17 -0400

    student_oops.py

--- student_oops.py
+++ student_oops.py
@@ -1,11 +1,17 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return (name, house)
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
