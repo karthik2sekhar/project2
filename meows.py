8ac2db249d77ec1f2333c7f6ab2bf0fe0a21c2a6
Karthik Sekhar <karthik2sekhar@gmail.com>
2025-06-22 23:14:51 -0400

    unpack.py

--- meows.py
+++ meows.py
@@ -1,8 +1,11 @@
-def meow(n: int) -> str:
-    return "meow\n" * n
+import argparse
+
+parser = argparse.ArgumentParser(description="meow like a cat")
+parser.add_argument("-n",default=1,type=int, help="Number of time to meow")
+args = parser.parse_args()
+
+for _ in range(args.n):
+    print("meow")
 
 
-number: int = int(input("Enter Number: "))
-meow: str = meow(number)
-print(meow, end="")
 
